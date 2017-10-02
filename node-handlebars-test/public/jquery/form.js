a03579bb498ea8683b94309dfc25d632a3ad34f9
daniel <daniel@danielpc>
2017-10-02 11:28:18 +0900

    password form

--- node-handlebars-test/public/jquery/form.js
+++ node-handlebars-test/public/jquery/form.js
@@ -1,10 +1,29 @@
 var FORM = {
-	init: function(){
-		console.log("ready");
+	validPass: function(str) {
+		var regex_pass = /^(?=.*\d)(?=.*[!@#$%^&+=])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+		return regex_pass.test(str);
+	},
+		
+	submit : function() {
+		$('div.alert-success').hide();
+		$('div.alert-info').hide();
+		var input_pass = $('#input_form').val();
+		if (FORM.validPass(input_pass)) {
+			$('div.alert-success').show();
+		} else {
+			$('div.alert-info').show();
+		}
+	},
+
+	init : function() {
+		$('#submit_form').on('click', function(event) {
+			event.preventDefault();
+			FORM.submit();
+		})
 	}
 };
 
-$(document).ready(function(){
+$(document).ready(function() {
 	COMMON.timeset();
 	FORM.init();
 });
